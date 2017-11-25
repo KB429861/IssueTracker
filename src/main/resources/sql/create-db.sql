a30ee1e6382251e6be2feea576749f7a98ea09f9
KB429861 <kb429861@gmail.com>
2017-11-25 17:00:40 +0700

    Script update.

--- src/main/resources/sql/create-db.sql
+++ src/main/resources/sql/create-db.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS issues (
   summary     NVARCHAR2(255) NOT NULL,
   description NVARCHAR2(4000),
   author      NVARCHAR2(255) NOT NULL,
-  start_date  DATE           NOT NULL,
+  start_date  SMALLDATETIME  NOT NULL,
   status      NVARCHAR2(30)  NOT NULL,
   PRIMARY KEY (id)
 );
